24b56ba967a178f8af0b60fadb564399602ac67b
Melissa Coronado <melissa_coronado@hotmail.com>
2021-01-13 22:41:53 -0300

    Desafio2.2

---
+++ Desafio2/opPermitidas.ts
@@ -0,0 +1,8 @@
+export const permitidas = function (n1: number, n2: number, op: string){
+    switch (op){
+        case "suma":{ return n1 + n2; }
+        case "resta":{ return n1 - n2; }
+        
+    }
+
+}
