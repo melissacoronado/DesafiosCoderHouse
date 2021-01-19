868c034f04e7a3bfcbf05c2bbcd4bd3ff4780b81
Melissa Coronado <melissa_coronado@hotmail.com>
2021-01-18 23:09:13 -0300

    Intento-Desafio3

--- Desafio3/JA+CoronadoMelissa.js
+++ Desafio3/JA+CoronadoMelissa.js
@@ -2,30 +2,52 @@ let Finalizar = (intInterval) =>{
     clearInterval(intInterval);
     console.log("Proceso Completo!");
 }
-
-let TextFunc = (palabra, interval, funcCallback)=>{//
-    interval = typeof(Interval) == "undefined" ? 1000 : Interval;
-    
-    let Arraypalabra = palabra.split(' ');
+//https://www.programiz.com/javascript/async-await
+//https://stackoverflow.com/questions/32445323/jquery-replace-text-every-2-seconds-with-words-from-an-array
+const ImprimirTexto = async (Arraypalabra, interval, funcCallback) =>{
     let i=0;
-    
-    let intervalID = setInterval(()=>{  
-        (!(i < Arraypalabra.length)) && funcCallback(intervalID);
-        console.log(Arraypalabra[i++]);  
-        /*if(!(i<Arraypalabra.length)){
-            console.log(Arraypalabra[i++]);  
-        }else{
-            funcCallback(intervalID);
-        }*/
-        /*if(i<Arraypalabra.length){
-            funcCallback(intInterval);
-        }else{
-
-        }
-        for(var i; i<Arraypalabra.length; i++){
-            console.log(Arraypalabra[i]);
-        }*/
+    let intervalID = setInterval(()=>{    
+        i++;  
+        
+            (!(i < Arraypalabra.length)) && funcCallback(intervalID);
+            console.log(i); 
+            console.log(`Palabra: ${Arraypalabra[i % Arraypalabra.length]}`);  
+            
     },interval)
+
 }
 
-TextFunc("Hola Mundo", null, Finalizar);
+let TextFunc = async (palabra, interval, funcCallback)=>{
+    interval = typeof(Interval) == "undefined" ? 1000 : Interval;    
+    let Arraypalabra = palabra.split(' ');
+    //await new Promise(resolve => setInterval(() => resolve(ImprimirTexto(Arraypalabra, interval, funcCallback)), interval));
+
+    try{
+        let word = await ImprimirTexto(Arraypalabra, interval, funcCallback);
+        console.log(100);
+    }catch(err){
+        console.log(err);
+    }
+    
+    //let i = 0;
+    /*var i= i || 0;
+    let intervalID;
+
+    if(i >= Arraypalabra.length){
+        funcCallback(intervalID);        
+    } else {
+        console.log(Arraypalabra[i]);
+        intervalID = setTimeout(TextFunc, interval, palabra, interval, ++i, funcCallback);
+    }*/
+
+    /*
+    let intervalID = setInterval(()=>{         
+        (!(i < Arraypalabra.length)) && funcCallback(intervalID);
+        console.log(`Palabra ${Arraypalabra[i % Arraypalabra.length]}`);  
+        i++;        
+    },interval)
+    */
+}
+
+ TextFunc("Hola Mundo 2021", null, Finalizar);
+ //TextFunc("Esta es otra prueba", null, Finalizar);
--- Desafio3/test.js
+++ Desafio3/test.js
@@ -11,4 +11,28 @@ function play(count) {
     }
   }
 
-  play();
+  //play();
+
+
+
+
+  let intervalid 
+async function testFunction() {
+    intervalid = setInterval(() => {
+        // I use axios like: axios.get('/user?ID=12345').then
+        new Promise(function(resolve, reject){
+            resolve(console.log('something'))
+        }).then(res => {
+            if (condition) {
+               // do something 
+               console.log(100);
+            } else {
+               clearInterval(intervalid)
+            }    
+        })  
+    }, 1000)  
+}
+// you can use this function like
+testFunction()
+// or stop the setInterval in any place by 
+//clearInterval(intervalid)
