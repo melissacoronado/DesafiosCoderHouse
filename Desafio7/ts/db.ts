4a697a72f0f4f642cf757012a2d8fb3994330262
Melissa Coronado <melissa_coronado@hotmail.com>
2021-02-02 18:28:35 -0300

    Ajustes

--- Desafio7/ts/db.ts
+++ Desafio7/ts/db.ts
@@ -6,6 +6,7 @@ const leerDB = async (fileName: string) =>{
       const data = await fs.promises.readFile(path.resolve(__dirname, fileName), { encoding: 'utf8' })
       return JSON.parse(data);
     } catch (error) {
+      console.log('Error Obteniendo datos de .txt')
       return [];
     }
   }
