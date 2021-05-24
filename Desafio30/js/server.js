fb3f6e0a95711211ab24432c1f817ca68300ca60
Melissa Coronado <melissa_coronado@hotmail.com>
2021-05-24 17:11:56 -0300

    Desafio30Nginx

--- Desafio30/js/server.js
+++ Desafio30/js/server.js
@@ -12,30 +12,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.HistoryMensajesChat = exports.opsChat = exports.opsProd = void 0;
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
-const ApiProductosRoute_1 = require("./rutas/ApiProductosRoute");
-const viewsRoute_1 = require("./rutas/viewsRoute");
-const bd_1 = require("./bd/bd");
-const archivos_1 = require("./bd/archivos");
+const ApiProductosRoute_1 = require("./routes/ApiProductosRoute");
+const viewsRoute_1 = require("./routes/viewsRoute");
+const authRoute_1 = require("./routes/authRoute");
+const productos_1 = require("./service/productos");
+const mensajes_1 = require("./service/mensajes");
+const db = require('./service/bd'); //Para conex a la bd moongose
+const session = require('express-session');
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const MongoStore = require('connect-mongo');
+const compression = require('compression');
+const logger_1 = require("./helper/logger");
 const handlebars = require('express-handlebars');
 const app = express_1.default();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, { autoConnect: false /*, transports: ['websocket']*/ });
-exports.opsProd = new bd_1.Producto();
-exports.opsChat = new archivos_1.ChatMsg("chatBD.txt");
-exports.HistoryMensajesChat = [];
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    exports.opsChat.ChatMessages = yield exports.opsChat.getMessages();
-}))();
+const optionsMongoAtlas = { useNewUrlParser: true, useUnifiedTopology: true };
+let opsProd = new productos_1.Producto();
+let opsChat = new mensajes_1.ChatMsg();
 let puerto = process.env.port || 8080;
-app.use('/api', express_1.default.static(__dirname + '/public')); //Al principio
+//Desafio30 //app.use('/api', express.static(__dirname + '/public')); //Al principio
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use('/api', viewsRoute_1.RouterViewsProductos);
-app.use('/api/productos', ApiProductosRoute_1.RouterApiProductos);
-//app.use("/scripts", express.static(__dirname + '/public/scripts'));
+app.use(compression());
 //Sets handlebars configurations 
 app.engine('hbs', handlebars({
     extname: ".hbs",
@@ -45,19 +46,50 @@ app.engine('hbs', handlebars({
 }));
 app.set('views', path_1.default.resolve(__dirname + '/public/views/'));
 app.set('view engine', 'hbs');
+app.use(cookie_parser_1.default());
+app.use(session({
+    store: new MongoStore({
+        mongoUrl: 'mongodb+srv://mcoronado:Europe03**@cluster0.ra8rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        mongoOptions: optionsMongoAtlas
+    }),
+    secret: '123456789',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: (60 * 100000) //10 minuto
+    }
+}));
+app.use('/api', viewsRoute_1.RouterViewsProductos);
+app.use('/api/productos', ApiProductosRoute_1.RouterApiProductos);
+app.use("/api/user", authRoute_1.AuthUsers);
+//app.use("/scripts", express.static(__dirname + '/public/scripts'));
 //app.set('scripts', express.static(path.resolve(__dirname + '/public/scripts/'))); 
 //app.set('socketio', io);
 http.listen(puerto, () => {
     console.log('Servidor escuchando en puerto 8080');
+    logger_1.logger.trace("Servidor escuchando en puerto 8080");
+    logger_1.logger.debug("Servidor escuchando en puerto 8080");
+    logger_1.logger.info("Servidor escuchando en puerto 8080");
+    logger_1.logger.warn("Servidor escuchando en puerto 8080");
 }).on("error", (err) => {
     console.log(err);
+    logger_1.logger.error(err);
+    logger_1.logger.fatal(err);
+});
+/*module.exports = {
+    log4js: logger
+  };*/
+//Desafio30
+app.get('/datos', (req, res) => {
+    res.send('Server express en ' + puerto + ' ProcessId ' + process.pid);
 });
 io.on('connection', (socket) => {
     let idSock = socket.id;
     let addedMail = false;
     console.log('A user connected' + socket.id);
     //chat
-    socket.on('New chatMsg', (data) => {
+    socket.on('New chatMsg', (data) => __awaiter(void 0, void 0, void 0, function* () {
+        //console.log(data);
         //Leer data
         const { mail, msg, time } = data;
         //Agregar usuario- asociar correo con socket
@@ -68,17 +100,19 @@ io.on('connection', (socket) => {
         const newMsg = { mail: mail,
             time: time,
             message: msg };
-        exports.opsChat.addMessage(newMsg);
+        yield opsChat.addMessage(newMsg);
         //Emit para mostrar en la lista
         io.emit('new message', newMsg);
-    });
+    }));
     //Productos
-    socket.on('dataProds', (data) => {
-        const { title, price, thumbnail } = data;
-        const newProduct = { title, price, thumbnail };
-        exports.opsProd.addProduct(newProduct);
+    socket.on('dataProds', (data) => __awaiter(void 0, void 0, void 0, function* () {
+        const { nombre, descripcion, codigo, foto, precio, stock } = data;
+        const timestamp = new Date(Date.now());
+        const newProduct = { timestamp, nombre, descripcion, codigo, foto, precio, stock };
+        console.log(newProduct);
+        yield opsProd.addProduct(newProduct);
         io.emit('ProductoAgregado', data);
-    });
+    }));
     socket.on('disconnect', () => {
         console.log(`Disconnected ${idSock}`);
     });
