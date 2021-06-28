154834199f5fbcca91b457606ad8d8943ad86e85
Melissa Coronado <melissa_coronado@hotmail.com>
2021-06-28 20:24:04 -0300

    D39

--- Desafio39/src/tests/mocha.test.ts
+++ Desafio39/src/tests/mocha.test.ts
@@ -7,6 +7,7 @@ describe("test de integración de Productos", function() {
 
     it('debería mostrar listado de productos existentes', async () => {
         let listado = await opsProd.showProducts();
+        //expect(listado).to.include.keys('data')
         assert.notEqual(listado.length, 0);
     })
 
